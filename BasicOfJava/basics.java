1f59d4fc11b5a7d7c122ff9bc2c15d04bfea9bfd
Mohd Hasan Noorani <151490688+hasannoorani1@users.noreply.github.com>
2024-01-18 21:08:33 +0530

    Add files via upload

---
+++ BasicOfJava/basics.java
@@ -0,0 +1,23 @@
+package BasicsOfJava;
+
+public class basics {
+    public static void main(String[] args) {
+
+
+        System.out.print("Hello");
+
+        System.out.print("Hasan");
+
+        System.out.println("\nHello");
+
+        System.out.println("World");
+
+        System.out.print("How are you \nI am fine");
+
+        // println --> new line
+        // \n --> new line
+
+
+    }
+
+}
