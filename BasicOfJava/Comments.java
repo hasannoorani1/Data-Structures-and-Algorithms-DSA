5145b2d7487836e6a5258dd20e21ad21b32e34d0
Mohd Hasan Noorani <151490688+hasannoorani1@users.noreply.github.com>
2024-01-18 20:14:32 +0530

    Add files via upload

---
+++ BasicOfJava/Comments.java
@@ -0,0 +1,23 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+
+       //  This is single line comments
+
+       // System.out.println("Hello");
+       // System.out.println("Hasan");
+       
+
+       /* This is multi line comments */
+       
+       /* 
+          System.out.println("Hello");
+          System.out.println("World");
+          System.out.println("Hello");
+          System.out.println("Hasan");
+        
+        */
+
+    }
+    
+}
