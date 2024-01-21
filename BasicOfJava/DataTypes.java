b18bb30008d8dfef89ada0a789f465e06163ac8d
Mohd Hasan Noorani <151490688+hasannoorani1@users.noreply.github.com>
2024-01-21 11:41:30 +0530

    Add files via upload

---
+++ BasicOfJava/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        //Primitive Data Types
+        int myNum = 5;
+        System.out.println(myNum);
+
+        float myFloatNum = 7.99f;
+        System.out.println(myFloatNum);
+
+        char myLetter = 'D';
+        System.out.println(myLetter);
+
+        boolean myBool = true;
+        System.out.println(myBool);
+
+        String myText = "Hello";
+        System.out.println(myText);
+       
+    }
+}
+
---
+++ BasicOfJava/Variables.java
@@ -0,0 +1,17 @@
+public class Variables{
+    public static void main(String[] args) {
+        
+        int age = 20;
+        age = 25;
+        System.out.println(age);
+
+        int x = 5, y = 10, z = 20;
+        System.out.println(x + y + z);
+
+        
+
+        //Constant
+        final double PI = 3.14;
+        System.out.println(PI); 
+ }
+}
